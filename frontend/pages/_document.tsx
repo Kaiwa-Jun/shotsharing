5dbe40125c34b317402b3cdc53fcedd81aa439f1
Kaiwa-Jun <kj.11235813213455@gmail.com>
2023-06-05 02:29:16 +0900

    OGPの設定

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="ja">
       <Head>
         {/* Global Site Tag (gtag.js) - Google Analytics */}
         <script
