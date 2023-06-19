9641c789e86f57bc1cc933ed05b231ab3cc7002e
Kaiwa-Jun <kj.11235813213455@gmail.com>
2023-06-19 21:38:17 +0900

    削除機能の修正（エンドポイントの修正）

--- frontend/utils/api/deletePhoto.ts
+++ frontend/utils/api/deletePhoto.ts
@@ -1,6 +1,6 @@
 export const deletePhoto = async (photoId: number) => {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/photos/${photoId}`,
+    `${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/v1/photos/${photoId}`,
     {
       method: "DELETE",
     }
