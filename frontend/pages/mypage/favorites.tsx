c6a15804de8c4f77531dc2e5633dded9ab2807b3
Kaiwa-Jun <kj.11235813213455@gmail.com>
2023-06-28 00:16:48 +0900

    mypageの修正

--- frontend/pages/mypage/favorites.tsx
+++ frontend/pages/mypage/favorites.tsx
@@ -4,7 +4,7 @@ import UserInfo from "@/components/organisms/UserInfo";
 
 const MyFavoritesPage: React.FC = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen mt-20">
       <main className="flex-grow" style={{ minHeight: "150vh" }}>
         <UserInfo />
         <MypageTab />
