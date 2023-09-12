436415bd8eee540783687e369d739f5a22b2e1c6
Kaiwa-Jun <kj.11235813213455@gmail.com>
2023-09-12 17:49:57 +0900

    echo行の削除

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -113,5 +113,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  Rails.logger.info("PORT is: #{ENV['PORT']}")
+  # Rails.logger.info("PORT is: #{ENV['PORT']}")
 end
