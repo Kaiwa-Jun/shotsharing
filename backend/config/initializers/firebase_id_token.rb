a5a31ebc69f47e61ab0c7401730d891e7bca2686
Kaiwa-Jun <kj.11235813213455@gmail.com>
2023-09-13 23:07:35 +0900

    redis関連をコメントアウト

--- backend/config/initializers/firebase_id_token.rb
+++ backend/config/initializers/firebase_id_token.rb
@@ -1,7 +1,7 @@
-FirebaseIdToken.configure do |config|
-  config.redis = Redis.new(url: ENV['REDIS_URL'])
-  config.project_ids = [Rails.application.secrets.firebase_project_id]
-end
+# FirebaseIdToken.configure do |config|
+#   config.redis = Redis.new(url: ENV['REDIS_URL'])
+#   config.project_ids = [Rails.application.secrets.firebase_project_id]
+# end
 
-# Update Google's certificates
-FirebaseIdToken::Certificates.request
+# # Update Google's certificates
+# FirebaseIdToken::Certificates.request
