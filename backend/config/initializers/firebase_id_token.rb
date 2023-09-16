9133b62767d381bc959a75d878c0d2cb271d94b5
Kaiwa-Jun <kj.11235813213455@gmail.com>
2023-09-16 16:51:05 +0900

    firebase_id_tokenのSSL証明書の検証を無効にする

--- backend/config/initializers/firebase_id_token.rb
+++ backend/config/initializers/firebase_id_token.rb
@@ -1,5 +1,5 @@
 FirebaseIdToken.configure do |config|
-  config.redis = Redis.new(url: ENV['REDIS_URL'])
+  config.redis = Redis.new(url: ENV['REDIS_URL'], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
   config.project_ids = [Rails.application.secrets.firebase_project_id]
 end
 
